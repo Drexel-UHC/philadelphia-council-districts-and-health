6bfe61b350605273577538c96ed578e445b28bf2
trushovi <tr842@drexel.edu>
2025-03-24 10:41:08 -0400

    fixed the way the population census blocks dataset was being saved

--- Data/R Scripts/Create_CCDistrict_total_pop.R
+++ Data/R Scripts/Create_CCDistrict_total_pop.R
@@ -26,7 +26,8 @@ joined_blocks <-merge(total_population_blocks, Blocks2020_CouncilDistrict2024,
                       by.x = "GEOID", by.y = "GEOID20", all.x = TRUE, all.y = TRUE)
 
 #save the block level dataset:
-save(joined_blocks, file="clean datasets/population_census_blocks")
+population_census_blocks<-joined_blocks
+save(population_census_blocks, file="clean datasets/population_census_blocks.Rdata")
 
 #aggregate to get district level population
 CCDistrict_pop<-joined_blocks %>% 
