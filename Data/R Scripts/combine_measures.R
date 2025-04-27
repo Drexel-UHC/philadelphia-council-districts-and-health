bd620700ca9234eff5f415d2cca58e129a66e7ff
invaderbunny <amberbolli@gmail.com>
2025-04-27 13:06:13 -0400

    Created Citywide averages

--- Data/R Scripts/combine_measures.R
+++ Data/R Scripts/combine_measures.R
@@ -51,6 +51,64 @@ council_measures<-Active_licenses %>%
   left_join(HVI_CCdistrict, by=c("DISTRICT")) %>% 
   left_join(Tree_canopy_CCdistrict, by=c("DISTRICT"))
 
+##Find City-wide Averages: Education, Plumb/Kitch, Owner/Renter, MedianAge, HHincome, Poverty, Race
+#Uninsured, 
+
+City_average <- council_measures %>% 
+  summarise(
+    city_totalpop = sum(CCDistrict_population),
+    city_totalpop25 = sum(total_pop_over25),
+    city_less_than_hs = sum(less_than_hs),
+    city_hs_grad = sum(hs_grad),
+    city_some_college = sum(some_college),
+    city_college_grad = sum(college_grad),
+    city_total_violations = sum(total_violations),
+    city_parcel_count = sum(parcel_count),
+    city_total_housing_units = sum(district_total_housingunits),
+    city_lack_plumb = sum(district_lack_plumbing),
+    city_lack_kitch = sum(district_lack_kitchen),
+    city_medianage = mean(district_median_age_total),
+    city_medianage_female = mean(district_median_age_female),
+    city_medianage_male = mean(district_median_age_male),
+    city_medianHHincome = mean(medianHH_income_district),
+    city_HH_total = sum(total_HH_district),
+    city_owner = sum(total_owner_district),
+    city_renter = sum(total_renter_district),
+    city_poor = sum(CD_Poor),
+    city_white = sum(white),
+    city_black = sum(black),
+    city_native = sum(native),
+    city_asian = sum(asian),
+    city_pi = sum(pi),
+    city_other = sum(other),
+    city_asian_pi = sum(asian_pi),
+    city_two_more = sum(two_more),
+    city_hispanic = sum(hispanic),
+    city_not_hispanic = sum(not_hispanic)
+  ) %>% 
+  mutate(
+    city_less_than_hs_pct = city_less_than_hs/city_totalpop25 * 100,
+    city_hs_grad_pct = city_hs_grad / city_totalpop25 * 100,
+    city_some_college_pct = city_some_college / city_totalpop25 * 100,
+    city_college_grad_pct = city_college_grad / city_totalpop25 * 100,
+    city_total_violations_pct = city_total_violations / city_parcel_count * 100, 
+    city_lack_plumb_pct = city_lack_plumb / city_total_housing_units * 100, 
+    city_lack_kitch_pct = city_lack_kitch / city_total_housing_units * 100, 
+    city_owner_pct = city_owner / city_HH_total * 100, 
+    city_renter_pct = city_renter / city_HH_total * 100, 
+    city_poor_pct = city_poor / city_totalpop * 100, 
+    city_white_pct = city_white / city_totalpop * 100,
+    city_black_pct = city_black / city_totalpop * 100,
+    city_native_pct = city_native / city_totalpop * 100,
+    city_asian_pct = city_asian / city_totalpop * 100,
+    city_asian_pi_pct = city_asian_pi / city_totalpop * 100,
+    city_pi_pct = city_pi / city_totalpop * 100,
+    city_other_pct = city_other / city_totalpop * 100,
+    city_two_more_pct = city_two_more / city_totalpop * 100,
+    city_hispanic_pct = city_hispanic / city_totalpop * 100,
+    city_not_hispanic_pct = city_not_hispanic / city_totalpop * 100
+  )
+
 
 #paired down dataset:
 council_measures_reduc<-council_measures %>% 
