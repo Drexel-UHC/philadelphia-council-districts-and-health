6c398e53189b3566b43dc402d625bd10722cd97e
rl627 <rl627@drexel.edu>
2025-05-09 14:09:23 -0400

    second pass at intro module #10

--- App/shiny/phl-council-health-app-v1/R/Modules/IntroductionModule.R
+++ App/shiny/phl-council-health-app-v1/R/Modules/IntroductionModule.R
@@ -10,21 +10,19 @@ IntroductionModule_UI <- function(id) {
   
   # Introduction section
   div(class = "section",
-    h2("Introduction", class = "section-title"),
-    p("Philadelphia has faced differences in health outcomes across neighborhoods – differences that reflect broader disparities in income, opportunity, and access to essential resources. These are not just statistics on a page - they are the city's residents, and they are shaped by where people live and the conditions of their community."),
+    h3("Introduction", class = "section-title"),
+    p("The health of Philadelphia residents varies drastically across the city – differences that reflect broader disparities in income, opportunity, and access to essential resources. These are not just personal choices made by the city’s residents – they are shaped by federal, state and local laws and policies."),
     
-    p("This project takes a closer look at those conditions by analyzing publicly available data and mapping key health indicators and social determinants of health across all 10 Philadelphia City Council Districts. By doing this, we aim to provide a clearer picture of how geography and disparities in health interact to shape the health of Philadelphians."),
+    p("This project takes a closer look at those conditions by analyzing publicly available data and mapping key health indicators and social determinants of health across all 10 Philadelphia City Council Districts. By doing this, we aim to provide a clearer picture of how politics and geography intersect to shape the health of Philadelphians."),
     
     p("Our goal is to equip all 17 Philadelphia City Council members and the public with actionable, district-level insights that can guide and empower more equitable policy and investment into our city. By connecting this data to City Council Districts, we hope this project continues to grow and support effective policy solutions that can promote equality and better health for all Philadelphians.")
   ),
   
   # Interactive elements section
   div(class = "section",
-    h2("Find your City Council District:", class = "section-title"),
-    # actionButton(ns("findDistrict"), "Find Your District", class = "btn-primary"),
-    
-    h2("How to Use:", class = "mt-4 section-title"),
-    p("To explore the data...")
+    p(tags$b("Find your City Council District: "), 
+      a("using this Philadelphia City Council Tool", href = "https://philacitycouncil.maps.arcgis.com/apps/instant/lookup/index.html?appid=9cf0fb3394914cd0a8a7f22ea1395d55", target = "_blank"))
+  
   )
 )
 }
