8605c6bb0b72dbc38c53913d9bcbfa8fe5e82651
rl627 <rl627@drexel.edu>
2025-05-13 09:58:58 -0400

    outro text updates as per @trushovi  #17

--- App/shiny/phl-council-health-app-v1/R/Modules/OutroModule.R
+++ App/shiny/phl-council-health-app-v1/R/Modules/OutroModule.R
@@ -21,9 +21,12 @@ OutroModule_UI <- function(id) {
     div(class = "section",
       h4("Links to Related Work", class = "subsection-title"),
       tags$ul(
-        tags$li(a("NYU Congressional Dashboard", href = "#", target = "_blank")),
-        tags$li("Congressional District Journal Articles"),
-        tags$li("Chicago Health Atlas")
+        tags$li(a("Congressional District Health Dashboard", href = "https:/www.congressionaldistricthealthdashboard.org", target = "_blank")),
+        tags$li(a("Rushovich T, Nethery RC, White A, Krieger N. Gerrymandering and the Packing and Cracking of Medical Uninsurance Rates in the United States. J Public Health Manag Pract. 2024", href = "https://pubmed.ncbi.nlm.nih.gov/39190647/", target = "_blank")),
+        tags$li(a("Schnake-Mahl A, Anfuso G, Goldstein ND, et al. Measuring variation in infant mortality and deaths of despair by US congressional district in Pennsylvania: a methodological case study. Am J Epidemiol. 2024", href = "https://pubmed.ncbi.nlm.nih.gov/38412272/", target = "_blank")),
+        tags$li(a("Schnake-Mahl A, Anfuso G, Bilal U, et al. Court-mandated redistricting and disparities in infant mortality and deaths of despair. BMC Public Health. 2025", href = "https://pmc.ncbi.nlm.nih.gov/articles/PMC11921522/", target = "_blank")),
+        tags$li(a("Schnake-Mahl A, Anfuso G, Hernandez SM, Bilal U. Geospatial Data Aggregation Methods for Novel Geographies: Validating Congressional District Life Expectancy Estimates. Epidemiology. 2025", href = "https://pubmed.ncbi.nlm.nih.gov/39329432/", target = "_blank")),
+        tags$li(a("Spoer BR, Chen AS, Lampe TM, et al. Validation of a geospatial aggregation method for congressional districts and other US administrative geographies. SSM Popul Health. 2023", href = "https://pmc.ncbi.nlm.nih.gov/articles/PMC10498302/", target = "_blank"))
       )
     ),
     
@@ -32,14 +35,14 @@ OutroModule_UI <- function(id) {
         h4("Citation", class = "subsection-title"),
         div(
           class = "code-block",
-          tags$code("Urban Health Collaborative, Health of Philadelphia City Council Districts Dashboard, 2025")
+          tags$code(HTML("Urban Health Collaborative, <em>Health of Philadelphia City Council Districts Dashboard</em>, 2025"))
         )
     ),
     
     # Contact section
     div(class = "section",
       h4("Contact Us", class = "subsection-title"),
-      p("Please reach out to ", tags$b("XXXXX"), " with any questions.")
+      p("Please reach out to ", a(href = "mailto:UHC@drexel.edu", "UHC@drexel.edu"), " with any questions.")
     )
   )
 }
