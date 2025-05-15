26ac20d6a11561fadc588ee1773997d9ab6f0f9d
Ran Li <45013044+ran-codes@users.noreply.github.com>
2025-05-15 13:50:32 -0400

    Header component claude reviewed #11

--- App/shiny/next-js-deploy/components/Header.tsx
+++ App/shiny/next-js-deploy/components/Header.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
 export default function Header() {
+  // Constants for reuse
+  const UHC_URL = "https://drexel.edu/uhc/";
+  const COLORS = {
+    lightGray: "#a7a8aa",
+    darkBlue: "#07294d"
+  };
 
-  // abstract the logo part of the header to an object here (as jsx so the <a> and <Image> tags
+  // abstract the logo part of the header to an object here
   const logo = (
-    <div 
-      className="col-span-12 md:col-span-4 flex items-center justify-center h-full"
-    >
-      <a
-        href="https://drexel.edu/uhc/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+    <div className="col-span-12 md:col-span-4 flex items-center justify-center h-full pt-4">
+      <a href={UHC_URL} target="_blank" rel="noopener noreferrer">
         <Image
           src="/logo.png"
           alt="Drexel UHC Logo"
@@ -22,37 +22,32 @@ export default function Header() {
         />
       </a>
     </div>
-  )
+  );
 
   const link = (
-       <div 
-       className="col-span-12 md:col-span-4  flex flex-col items-center">
-          <div>
-            <span className="text-[11px] text-[#a7a8aa]">Learn more</span>
-            <br />
-    
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[16px] font-[900] text-[#07294d] no-underline"
-            href="https://drexel.edu/uhc/"
-          >
-            https://drexel.edu/uhc/
-          </a>
-          </div>
-        </div>
-  )
-
+    <div className="col-span-12 md:col-span-4 flex flex-col items-center">
+      <div>
+        <span className={`text-[11px] text-[${COLORS.lightGray}]`}>Learn more</span>
+        <br />
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`text-[16px] font-[900] text-[${COLORS.darkBlue}] no-underline`}
+          href={UHC_URL}
+        >
+          {UHC_URL}
+        </a>
+      </div>
+    </div>
+  );
 
   return (
     <header>
-      <div className="grid grid-cols-12 items-center p-[5px] ">
+      <div className="grid grid-cols-12 items-center p-[5px]">
         {logo}
-        <div className="col-span-0 md:col-span-4  "></div>
+        <div className="col-span-0 md:col-span-4"></div>
         {link}
       </div>
     </header>
   );
-
-  
 }
